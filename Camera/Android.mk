a428f2fed396e50f64c274d4b7c766b9a7133869
KuzonCode <bcsmithies@gmail.com>
2022-03-01 09:22:41 +1000

    Remove Sub Directory for APK

--- Camera/Android.mk
+++ Camera/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := Camera
-LOCAL_SRC_FILES := $(LOCAL_PATH)/apk/net.sourceforge.opencamera.apk
+LOCAL_SRC_FILES := net.sourceforge.opencamera.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := platform
